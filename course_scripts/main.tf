4334e4bae04e0336fa913f041c7387253f70cd4f
Stephen Granger <viper233@gmail.com>
2019-02-05 11:54:15 -0800

    Include boto3 library for running s3_sync Ansible module. Attempt to make Ansible plays idempotent.

--- course_scripts/main.tf
+++ course_scripts/main.tf
@@ -455,6 +455,7 @@ cat <<EOF > userdata
 #!/bin/bash
 /usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
 /bin/touch /var/spool/cron/root
+sudo /usr/bin/yum install -y python27-boto3
 sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/' >> /var/spool/cron/root
 EOF
 EOT
